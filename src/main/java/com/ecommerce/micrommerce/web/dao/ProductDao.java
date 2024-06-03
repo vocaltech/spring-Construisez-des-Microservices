c20a225e3e2a24f856b5bc402259c20a4352bb2e
vocaltech <it@vocaltech.fr>
2024-06-04 00:12:23 +0200

    Clean imports

--- src/main/java/com/ecommerce/micrommerce/web/dao/ProductDao.java
+++ src/main/java/com/ecommerce/micrommerce/web/dao/ProductDao.java
@@ -2,8 +2,6 @@ package com.ecommerce.micrommerce.web.dao;
 
 import com.ecommerce.micrommerce.web.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
