9c67a8f3a6f88fab10bf38d5b165e9539e565bb1
vocaltech <it@vocaltech.fr>
2024-06-05 18:28:00 +0200

    Add method findByOrderByNomAsc()

--- src/main/java/com/ecommerce/micrommerce/web/dao/ProductDao.java
+++ src/main/java/com/ecommerce/micrommerce/web/dao/ProductDao.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface ProductDao extends JpaRepository<Product, Integer> {
     Product findById(int id);
     List<Product> findByPrixGreaterThan(int prixLimit);
-
+    List<Product> findByOrderByNomAsc();
 }
